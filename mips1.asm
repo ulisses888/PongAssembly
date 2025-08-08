91146e3729a51124344bd406bc3c9e4d2ab3d3e7
ulisses888 <159669337+ulisses888@users.noreply.github.com>
2025-08-08 17:52:49 -0300

    Update mips1.asm

--- mips1.asm
+++ mips1.asm
@@ -1,6 +1,13 @@
 # endereco base 0x10010000
-li $t0,0x10010000
-li $a2,1
+
+.data
+	bitmap: .space 4096 
+	tamanho: .word 4
+	vetor: .word 0xFF000000, 0xFFFFFFFF, 0xff0022, 0x0400ff #Preto,Branco,Vermelho e Azul
+	
+.text
+
+jal reset
 
 
 
@@ -14,23 +21,56 @@ li $a2,1
 li $v0,10
 syscall
 
-pintar: #a0 = x, a1 = y a2 = cor (0,preto,1 branco)
-move $t7,$t0 # pega o endereco base
-#calcular o y
-li $t6,2048
-mult $a1,$t6
-mflo $t6
-add $t7,$t7,$t6 #t7 recebe o y
-li $t6, 4
-mult $t6,$a0
-mflo $t6
-add $t7,$t7,$t6 # + endereco x
-beqz $a2,preto
+bordas:
+
+
+
+
+
+reset:
+li $a0,0
+li $a1,0
+li $a2,0
+addi $sp,$sp,-4
+sw $ra,($sp)
+loopReset:
+jal pintar
+addi $a0,$a0,1
+beq $a0,32,incrementaY
+j loopReset
+
+incrementaY:
+li $a0,0
+addi $a1,$a1,1
+beq $a1,32,fimReset
+j loopReset
+
+fimReset:
+lw $ra,($sp)
+addi $sp,$sp,4
+jr $ra
+
+pintar: # a0 = x, a1 = y, a2 = cor (0 = preto, 1 = branco)
+    move $t7, $t0           # endereço base do bitmap display em $t7
+
+    li $t6, 32             
+    mult $a1, $t6          
+    mflo $t6
+    add $t6, $t6, $a0       
+
+    li $t5, 4
+    mult $t6, $t5           
+    mflo $t6
+
+    add $t7, $t7, $t6       
+
+    beqz $a2, preto         # se a2 == 0, cor preta
 branco:
-li $t5,0xFFFFFFFF
-sw $t5,($t7)
-jr $ra
+    li $t5, 0xFFFFFFFF      # branco
+    sw $t5, 0($t7)
+    jr $ra
 preto:
-li $t5, 0xFF000000
-sw $t5,($t7)
-jr $ra
+    li $t5, 0xFF000000      # preto
+    sw $t5, 0($t7)
+    jr $ra
+
