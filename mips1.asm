2f81cff8a7f5823ab727cb0350746cb25bc7c72d
ulisses888 <159669337+ulisses888@users.noreply.github.com>
2025-08-09 01:55:54 -0300

    Update mips1.asm

--- mips1.asm
+++ mips1.asm
@@ -1,58 +1,116 @@
 # endereco base 0x10010000
 
 .data
-	bitmap: .space 4096 
+	bitmap: .space 4096
 	tamanho: .word 4
 	vetor: .word 0xFF000000, 0xff873800, 0xff0022, 0x0400ff #Preto,Marrom,Vermelho e Azul
-	
 .text
 li $t0,0x10010000 #endereco base
+# Endereços MMIO
+li $s6, 0xFFFF0000   # Keyboard Control
+li $s7, 0xFFFF0004   # Keyboard Data
 
-#jal reset
+
 jal reset
 jal bordas
+
+li $s0,13 #base p1
+li $s1,13 #base p2
+#tamanho raquete 5
+
 jal raquetes
+j loopPrincipal
 
 
+loopPrincipal:
+lw   $t0, 0($s6)      # Lê Keyboard Control
+andi $t0, $t0, 1
+beq  $t0, $zero, atualizar
 
+# Lê a tecla
+lw   $t1, 0($s7)      # ASCII da tecla
 
+# Compara com 'w'
+li   $t2, 'w'
+beq  $t1, $t2, player1Cima
 
+# Compara com 's'
+li   $t2, 's'
+beq  $t1, $t2, player1Baixo
 
+# Compara com 'o'
+li   $t2, 'o'
+beq  $t1, $t2, player2Cima
+
+# Compara com 'l'
+li   $t2, 'l'
+beq  $t1, $t2, player2Baixo
+
+atualizar:
+# Aqui pode atualizar a bola, IA do jogador 2, etc.
+j loopPrincipal
 
 li $v0,10
 syscall
+##############
+
+player1Cima:
+beq $s0,1,fimMovimento
+
+addi $s0,$s0,-1
+    
+	
+    
+
+fimMovimento:
+    jr $ra
+
+player1Baixo:
+
 
 ##############
-raquetes: #13-17
+
+player2Cima:
+
+player2Baixo:
+
+##############
+raquetes: #13-17 xp1 - 1 x p2 - 30 bases em s0 e s1
+addi $sp,$sp,-4
+sw $ra,($sp)
+
+li $t8,0 # contador
+# a0 = x, a1 = y, a2 = cor (0 = preto, 1 = marrom, 2 = vermelho, 3 = azul)
+#desenhar p1
+
 li $a0,1
-li $a1,13
-li $a2 2
+move $a1,$s0
+li $a2,2
+desenharp1:
 jal pintar
-li $a1,14
-jal pintar
-li $a1,15
-jal pintar
-li $a1,16
-jal pintar
-li $a1,17
-jal pintar
-
+addi $a1,$a1,1
+addi $t8,$t8,1
+beq $t8,5,proxDesenho
+j desenharp1
+#####
+proxDesenho:
+li $t8,0
 li $a0,30
-li $a1,13
-li $a2 3
+move $a1,$s1
+li $a2,3
+#desenhar p2
+desenharp2:
 jal pintar
-li $a1,14
-jal pintar
-li $a1,15
-jal pintar
-li $a1,16
-jal pintar
-li $a1,17
-jal pintar
+addi $a1,$a1,1
+addi $t8,$t8,1
+beq $t8,5,fimDesenhar
+j desenharp2
 
 
-
-
+fimDesenhar:
+lw $ra,($sp)
+addi $sp,$sp,4
+jr $ra
 ##############
 
 ##############
