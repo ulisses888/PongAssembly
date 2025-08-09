b3ca0455ee80e0516684529a5377c9a83dcba62e
ulisses888 <159669337+ulisses888@users.noreply.github.com>
2025-08-08 21:58:20 -0300

    Update mips1.asm

--- mips1.asm
+++ mips1.asm
@@ -3,14 +3,15 @@
 .data
 	bitmap: .space 4096 
 	tamanho: .word 4
-	vetor: .word 0xFF000000, 0xFFFFFFFF, 0xff0022, 0x0400ff #Preto,Branco,Vermelho e Azul
+	vetor: .word 0xFF000000, 0xff873800, 0xff0022, 0x0400ff #Preto,Marrom,Vermelho e Azul
 	
 .text
+li $t0,0x10010000 #endereco base
 
+#jal reset
 jal reset
-
-
-
+jal bordas
+jal raquetes
 
 
 
@@ -21,12 +22,71 @@ jal reset
 li $v0,10
 syscall
 
+##############
+raquetes: #13-17
+li $a0,1
+li $a1,13
+li $a2 2
+jal pintar
+li $a1,14
+jal pintar
+li $a1,15
+jal pintar
+li $a1,16
+jal pintar
+li $a1,17
+jal pintar
+
+li $a0,30
+li $a1,13
+li $a2 3
+jal pintar
+li $a1,14
+jal pintar
+li $a1,15
+jal pintar
+li $a1,16
+jal pintar
+li $a1,17
+jal pintar
+
+
+
+
+##############
+
+##############
 bordas:
+li $a0,0
+li $a1,0
+li $a2,1
+addi $sp,$sp,-4
+sw $ra,($sp)
 
+loopBordas:
+jal pintar
+addi $a0,$a0,1
+beq $a0,32,bordaInferior
+j loopBordas
 
+bordaInferior:
+li $a2,1
+li $a0,0
+li $a1,31
 
+loopBordas2:
+jal pintar
+addi $a0,$a0,1
+beq $a0,32,fimBordas
+j loopBordas2
 
+fimBordas:
+lw $ra,($sp)
+addi $sp,$sp,4
+jr $ra
+#############
 
+#############
 reset:
 li $a0,0
 li $a1,0
@@ -49,28 +109,26 @@ fimReset:
 lw $ra,($sp)
 addi $sp,$sp,4
 jr $ra
+###############
 
-pintar: # a0 = x, a1 = y, a2 = cor (0 = preto, 1 = branco)
-    move $t7, $t0           # endereço base do bitmap display em $t7
+###########
+pintar: # a0 = x, a1 = y, a2 = cor (0 = preto, 1 = marrom, 2 = vermelho, 3 = azul)
 
-    li $t6, 32             
-    mult $a1, $t6          
-    mflo $t6
-    add $t6, $t6, $a0       
+li $t7,0x10010000 
+li $t6, 32 #numero de pixeis linha       
+mult $a1, $t6          
+mflo $t6
+add $t6, $t6, $a0       
+li $t5, 4
+mult $t6, $t5           
+mflo $t6
+add $t7, $t7, $t6
 
-    li $t5, 4
-    mult $t6, $t5           
-    mflo $t6
-
-    add $t7, $t7, $t6       
-
-    beqz $a2, preto         # se a2 == 0, cor preta
-branco:
-    li $t5, 0xFFFFFFFF      # branco
-    sw $t5, 0($t7)
-    jr $ra
-preto:
-    li $t5, 0xFF000000      # preto
-    sw $t5, 0($t7)
-    jr $ra
-
+mult $a2,$t5
+mflo $t5
+la $t4,vetor
+add $t5,$t4,$t5
+lw $t5,($t5)
+sw $t5, 0($t7)
+jr $ra                    
+############
